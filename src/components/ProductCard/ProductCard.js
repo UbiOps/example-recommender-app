96658c3053b5a807ecd1072d7ff7b99e0c7be382
Mina Amrouche <mina.amrouche@dutchanalytics.com>
2021-02-24 14:39:39 +0100

    Create Price component

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,17 +1,8 @@
 import "./ProductCard.css";
-import { Link } from "components";
+import { capitalizeFirstLetter, generatePrice } from "utils";
+import { Link, Price } from "components";
 import { memo } from "react";
 import productImage from "assets/images/product.png";
-
-const capitalizeFirstLetter = (string) => {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-};
-
-const generatePrice = () => {
-  const rowPrice = Math.random() * 10 + 1;
-  const roundedPrice = Math.round(rowPrice * 100) / 100;
-  return roundedPrice.toFixed(2);
-};
 
 const ProductCard = ({ product, className, id, ...props }) => (
   <Link
@@ -26,7 +17,7 @@ const ProductCard = ({ product, className, id, ...props }) => (
     />
     <div className="product-card__details">
       <h4>{capitalizeFirstLetter(product)}</h4>
-      <h3 className="product-card__price">€ {generatePrice().toString()}</h3>
+      <Price>€ {generatePrice().toString()}</Price>
     </div>
   </Link>
 );
--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,8 @@ import Header from "./Header/Header";
 import IconButton from "./IconButton/IconButton";
 import Link from "./Link/Link";
 import NavBar from "./NavBar/NavBar";
+import Price from "./Price/Price";
 import ProductCard from "./ProductCard/ProductCard";
 import ProductsList from "./ProductsList/ProductsList";
 
-export { Header, IconButton, Link, NavBar, ProductCard, ProductsList };
+export { Header, IconButton, Link, NavBar, Price, ProductCard, ProductsList };
